3ed718e9a540c349b954a7858945905f24caf6b2
LiamGunning07 <liamgunning07@gmail.com>
2024-07-08 10:50:42 -0600

    Setting up font import

---
+++ frontend/src/app/ui/font.js
@@ -0,0 +1,10 @@
+import { JustAnotherHand } from '@next/font/google';
+
+const justAnotherHand = JustAnotherHand({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const justAnotherHandClass = justAnotherHand.className;
+
