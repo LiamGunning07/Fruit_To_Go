6cb2b0cb00931d2e2539c20ad28f75a45a76c55c
LiamGunning07 <liamgunning07@gmail.com>
2024-07-22 15:30:24 -0600

    Added styles for fruitcard

--- frontend/src/app/components/FruitCard.jsx
+++ frontend/src/app/components/FruitCard.jsx
@@ -7,8 +7,10 @@ export default function FruitCard({ title, price, img, quantity, description}) {
           <h1 className={styles.title}>{title}</h1>
           <h2 className={styles.price}>${price}</h2>
           <img src={img} className={styles.img} alt={title} />
-          <p className={styles.quantity}> Quantity:{quantity}</p>
           <p className={styles.description}>{description}</p>
+          <div classname={styles.overlay}>
+            <button className={styles.addToCart}>Add to Cart</button>
+          </div>
         </div>
   );
 };
