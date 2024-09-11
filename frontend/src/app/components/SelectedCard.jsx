2a428aa42290cbe82cd08a413ed7aa0616a3ba25
LiamGunning07 <liamgunning07@gmail.com>
2024-09-11 14:13:16 -0600

    Selected card state integrated needs functionality

--- frontend/src/app/components/SelectedCard.jsx
+++ frontend/src/app/components/SelectedCard.jsx
@@ -1,7 +1,10 @@
+import { useGlobalState } from "../context/GlobalStateContext";
 import FruitCard from "./FruitCard";
 import FruitList from "./FruitList";
 
 export default function SelectedCard() {
+const { fruits , selectedCard } = useGlobalState();
+
   return (
     <>
       <FruitCard />
--- frontend/src/app/fruit/page.js
+++ frontend/src/app/fruit/page.js
@@ -2,6 +2,7 @@
 import FilterDropDown from '../components/FilterDropDown'
 import FruitList from '../components/FruitList'
 import Header from '../components/Header'
+import SelectedCard from '../components/SelectedCard'
 import { useGlobalState } from '../context/GlobalStateContext'
 import styles from '../styles/Fruit.module.css'
 import '../styles/globals.css'
@@ -9,7 +10,7 @@ import '../styles/globals.css'
 
 export default function Fruit () {
 const {fruits, setFruits, selectedCard, setSelectedCard
-} = useGlobalState([])
+} = useGlobalState()
 
   return (
     <>
@@ -20,10 +21,23 @@ const {fruits, setFruits, selectedCard, setSelectedCard
       <h1> All Fruits </h1>
       <FilterDropDown/>
     </div>
-    <FruitList 
-    fruits={fruits}
-    setFruits={setFruits} />
+    {/* If selectedCard has something, render both selectedCard and FruitList */}
+  {selectedCard && selectedCard.length >= 0 ? (
+    <>
+      <SelectedCard selectedCard={selectedCard} />
+      {console.log("Inside selectedCard", selectedCard)}
+      <FruitList 
+        fruits={fruits}
+        setFruits={setFruits} 
+      />
     </>
-  )
-
-}
+  ) : (
+    /* If nothing is selected, render only the FruitList */
+    <FruitList 
+      fruits={fruits}
+      setFruits={setFruits} 
+    />
+  )}
+  </>
+  );
+  }
